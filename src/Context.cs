bdc03ef49b22e1e82aa26e8d22df13a12b4dbd05
Mithdring <3172765+mithdring@users.noreply.github.com>
2022-02-08 13:10:55 -0500

    feat: added subject to context

--- src/Context.cs
+++ src/Context.cs
@@ -3,6 +3,13 @@ namespace BDDContext;
 public abstract class Context<TSubject, TContext>
     where TContext : Context<TSubject, TContext>
 {
+    protected Context(TSubject subject)
+    {
+        Subject = subject;
+    }
+
+    public TSubject Subject { get; }
+
     public TContext With(Action<TContext> action)
     {
        var context = (TContext)this;
